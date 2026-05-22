730a3e5eca3eb29e9ca3360b198a32c77f91ace2
Bixam Tarala <bixam.tarala@gmail.com>
2026-05-22 21:30:20 +0530

    feat: rebuild mobile app shell and backend flows

---
+++ mobile_app_flutter/android/app/src/main/kotlin/com/croppulse/croppulse_mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.croppulse.croppulse_mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ mobile_app_flutter/android/app/src/main/kotlin/com/croppulse/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.croppulse.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
